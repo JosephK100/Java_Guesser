2e5bed132afbafe9a0cbed3b17828199c5d87f18
Joseph Kloepper <Thunderpop57@Gmail.com>
2026-03-13 22:48:43 +0000

    Final changes

--- Guesser.java
+++ Guesser.java
@@ -5,12 +5,18 @@
 import java.util.Scanner;
 
 public class Guesser{
+	//Scanner for all input
 	Scanner kb = new Scanner(System.in);
+	/*
+	 * My constructor, which displays menu and processes user choices.
+	 * Loops until user chooses to exit.
+	 */
 	public Guesser(){
 		Scanner kb = new Scanner(System.in);
 		int choice = -1;
 		boolean keepGoing = true;
-	
+		
+		//Java version of keepGoing
 		while (keepGoing){
 			printMenu();
 			choice = kb.nextInt();
@@ -23,18 +29,24 @@ public class Guesser{
 				computerGuesser();
 			}
 			else if (choice == 0){
-				keepGoing = false;
+				keepGoing = false; //Exit the program loop.
 		}
 	}
     }
-
+	/*
+	 * Prints the menu options (0-2)
+	 */
 	public void printMenu(){
 		System.out.println("0) Exit");
 		System.out.println("1) Human Guesser");
 		System.out.println("2 Computer Guesser");
 		System.out.println("Please enter 0-2: ");
 	}
-	
+	/*
+	 * For the human guesser:
+	 * The computer picks a random number from 1-100
+	 * The user has 7 chances to guess it.
+	 */
 	public void humanGuesser(){
 		int guesses = 0;
 		int userGuess = 0;
@@ -42,10 +54,12 @@ public class Guesser{
 		System.out.println("Let's play a guessing game.");
 		
 		boolean correct = false;
+		//User gets up to 7 guesses
 		while (guesses < 7 && !correct){
 			System.out.print("What is your guess? : ");
 			userGuess = kb.nextInt();
 
+			//Feedback for the user 
 			if (userGuess < answer){
 				System.out.println("That's too low.");
 			} else if (userGuess > answer){
@@ -60,25 +74,33 @@ public class Guesser{
 			System.out.println("You're out of guesses.");
 		}	
 	}
-	
+	/*
+	 * Computer guesser
+	 * H = The guess is too high
+	 * L = The guess is too low
+	 * C = The guess is correct
+	 */
 	public void computerGuesser(){
 		int low = 1;
 		int high = 100;
 		int attempt = 0;
 		boolean correct = false;
 
+		//Computer also gets up to 7 attempts
 		while (attempt < 7 && !correct){
+			//Guesses the midpoint of the current range
 			int guess = (low + high) / 2;
 			System.out.println("I guess " + guess + ".");
 			System.out.print("Is your number (H)igher, (L)ower, or (C)orrect? ");
 			String response = kb.next().toUpperCase();
 
+			//Changes the guess range based on user feedback
 			if (response.equals("H")){
-				low = guess + 1;
+				low = guess + 1;//Too low
 			} else if (response.equals("L")){
-				high = guess - 1;
+				high = guess - 1;//Too high
 			} else if (response.equals("C")){
-				System.out.println("I got it!");
+				System.out.println("I got it!");//Guessed correctly
 				correct = true;
 			}
 			
@@ -89,6 +111,7 @@ public class Guesser{
 		}
 	}
 
+	//Main method
 	public static void main(String[] args){
 		new Guesser();
 	}
