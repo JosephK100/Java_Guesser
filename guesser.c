bf348a2e3c8aa66b74b55ef53cd8f17c40f05aac
Joseph Kloepper <Thunderpop57@Gmail.com>
2026-03-13 05:13:33 +0000

    intial changes

--- guesser.c
+++ guesser.c
@@ -1,2 +1,14 @@
-test:wq
+//Joseph Kloepper
+//3/13/26
+//CS121
+//Java guesser 
+import java.util.Scanner;
 
+public class Guesser{
+	public Guesser(){
+	}
+
+	public static void main(String[] args){
+		new Guesser();
+	}
+}
